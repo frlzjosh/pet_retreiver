97eeb06d6ddb68cd203253f265932b96eee5dff4
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-08-29 21:57:19 -0700

    creating login page and imported material-ui and google maps for vue

---
+++ client/src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
+let api_key ='AIzaSyD75yziFepXfsT_zyNAuaAPSxUNVLlYK-w'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: api_key,
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+ 
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+})
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ client/src/views/Hub.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="hub">
+        <GmapMap
+            :center="{lat:34.238159, lng:-118.529460}"
+            :zoom="15"
+            map-type-id="terrain"
+            style="width: 500px; height: 300px"
+        >
+        </GmapMap>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Hub',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="scss">
+    .hub{
+        background-color: #012f70;
+    }
+</style>
