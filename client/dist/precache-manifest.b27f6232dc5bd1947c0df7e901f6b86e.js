d7039ad5705959bfc6bee46c7c888a5ea7c3770b
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-09-05 13:36:17 -0700

    taking out dist folder from gitignore

---
+++ client/dist/precache-manifest.b27f6232dc5bd1947c0df7e901f6b86e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "41d17e6cd7ff63e1d33f",
+    "url": "/js/chunk-vendors.66650d63.js"
+  },
+  {
+    "revision": "d828649a7d17b561cfa6",
+    "url": "/js/app.6e50bbf1.js"
+  },
+  {
+    "revision": "277d0c7b786f0190bccbcc5bd4594ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41d17e6cd7ff63e1d33f",
+    "url": "/css/chunk-vendors.dbd8845b.css"
+  },
+  {
+    "revision": "d828649a7d17b561cfa6",
+    "url": "/css/app.180b5b60.css"
+  },
+  {
+    "revision": "5f52dbefcf63c9f677ace09fcf8bc7d8",
+    "url": "/_redirects"
+  }
+];
